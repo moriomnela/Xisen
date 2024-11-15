f77aaeecde6e41eaeb2b766678a4d26b7a3b66b9
Moriom Nela <moriomislamnela23@gmail.com>
2024-11-15 22:47:02 +0600

    Xisen

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,16 +72,19 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
-	dots:true,
+	dots:true,                                                                          
     responsive:{
         0:{
-            items:1
+            items:1   
+
         },
         600:{
             items:3
+
         },
         1000:{
             items:5
+			
         }
     }
 })
